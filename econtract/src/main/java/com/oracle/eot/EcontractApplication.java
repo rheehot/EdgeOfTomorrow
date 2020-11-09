7b1426503abce884729f79a26012ab350852c4a1
jonggyoukim <jonggyoukim@10.0.1.19>
2020-11-09 14:31:46 +0900

    init

---
+++ econtract/src/main/java/com/oracle/eot/EcontractApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.eot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcontractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcontractApplication.class, args);
+	}
+
+}
