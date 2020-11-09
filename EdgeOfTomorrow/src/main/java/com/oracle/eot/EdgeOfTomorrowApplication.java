029d955a5e83f34da82077791998ca9e0c2219f2
jonggyoukim <jonggyoukim@10.0.1.19>
2020-11-09 14:24:37 +0900

    init

---
+++ EdgeOfTomorrow/src/main/java/com/oracle/eot/EdgeOfTomorrowApplication.java
@@ -0,0 +1,13 @@
+package com.oracle.eot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeOfTomorrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeOfTomorrowApplication.class, args);
+	}
+
+}
---
+++ EdgeOfTomorrow/src/test/java/com/oracle/eot/EdgeOfTomorrowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracle.eot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeOfTomorrowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
