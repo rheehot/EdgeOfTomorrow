8cf67f5d3144086da842a614f111f62e81153173
jonggyoukim <jonggyoukim@10.0.1.19>
2020-11-16 20:57:48 +0900

    ignore for /download

--- econtract/src/main/java/com/oracle/eot/login/SecurityConfig.java
+++ econtract/src/main/java/com/oracle/eot/login/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +17,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private EotUserDetailsService eotUserDetailsService;
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/download/**");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
